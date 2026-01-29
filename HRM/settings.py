8f988837d8c9b0d38bd4113948249016122a87f0
Jouhar <jouharparvez123@gmail.com>
2026-01-29 18:47:36 +0530

    Fix ALLOWED_HOSTS syntax in settings.py

--- HRM/settings.py
+++ HRM/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-f&d=vo+-agw)nl0m39be%u*@ov3e^@6sgfel0byrehadq_3(bx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
